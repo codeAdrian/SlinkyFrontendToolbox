0010cd36e91189b16bb68807ea7549f70d617102
codeAdrian <adrianbece@live.com>
2018-06-17 20:48:29 +0200

    Update

--- gulp.config.js
+++ gulp.config.js
@@ -1,87 +1,89 @@
-module.exports = function() {
-	var config = {
-		gulp: {
-			consoleDivider:
-				"---------------------------------------------------------------------"
-		},
+module.exports = function () {
+    var config = {
+        gulp: {
+            consoleDivider:
+                '---------------------------------------------------------------------'
+        },
 
-		fonts: {
-			input: "src/fonts/**/*.*",
-			output: "fonts/"
-		},
+        fonts: {
+            input: 'src/fonts/**/*.*',
+            output: 'fonts/'
+        },
 
-		sprites: {
-			input: "src/spritesheet/**/*.svg",
-			output: "src/",
-			filter: "**/*.svg",
-			config: {
-				cssFile: "css/vendor/_sprites.pcss",
-				preview: false,
-				templates: { scss: true },
-				padding: 10,
-				svgPath: "../images/spritesheet/sprites-min.svg",
-				pngPath: "../images/spritesheet/sprites-min.png",
-				svg: {
-					sprite: "images/spritesheet/sprites.svg"
-				}
-			}
-		},
+        sprites: {
+            input: 'src/spritesheet/**/*.svg',
+            output: 'src/',
+            filter: '**/*.svg',
+            config: {
+                cssFile: 'css/vendor/_sprites.pcss',
+                preview: false,
+                templates: { scss: true },
+                padding: 10,
+                svgPath: '../images/spritesheet/sprites-min.svg',
+                pngPath: '../images/spritesheet/sprites-min.png',
+                svg: {
+                    sprite: 'images/spritesheet/sprites.svg'
+                }
+            }
+        },
 
-		favicons: {
-			input: "src/favicon.png",
-			output: "images/favicon/",
-			config: {
-				appName: "Webite",
-				appDescription: "This is my website",
-				developerName: "Inchoo",
-				developerURL: "https://www.inchoo.net/",
-				background: "#fff",
-				path: "images/favicon/",
-				url: "https://www.project.loc",
-				display: "standalone",
-				orientation: "portrait",
-				start_url: "/?homescreen=1",
-				version: 1.0,
-				logging: false,
-				online: false,
-				html: "favicons.html",
-				pipeHTML: true,
-				replace: true
-			}
-		},
+        favicons: {
+            input: 'src/favicon.png',
+            output: 'images/favicon/',
+            config: {
+                appName: 'Webite',
+                appDescription: 'This is my website',
+                developerName: 'Inchoo',
+                developerURL: 'https://www.inchoo.net/',
+                background: '#fff',
+                path: 'images/favicon/',
+                url: 'https://www.project.loc',
+                display: 'standalone',
+                orientation: 'portrait',
+                start_url: '/?homescreen=1',
+                version: 1.0,
+                logging: false,
+                online: false,
+                html: 'favicons.html',
+                pipeHTML: true,
+                replace: true
+            }
+        },
 
-		css: {
-			inputMain: "src/css/**/[^_]*.{css,pcss}",
-			inputAll: "src/css/**/*.{css,pcss}",
-			excludeVendor: "!src/css/vendor/**/*",
-			output: "css/",
-			config: {
-				atImport: { extensions: [".css", ".pcss"], prefix: "_" },
-				cssNext: {
-					browsers: ["last 3 versions", "not ie<10"],
-					cascade: false
-				},
-				cssNano: { autoprefixer: false }
-			}
-		},
+        css: {
+            inputMain: 'src/css/**/[^_]*.{css,pcss}',
+            inputAll: 'src/css/**/*.{css,pcss}',
+            excludeVendor: '!src/css/vendor/**/*',
+            output: 'css/',
+            config: {
+                atImport: { extensions: ['.css', '.pcss'], prefix: '_' },
+                postcssPresetEnv: {
+                    browsers: ['last 3 versions', 'not ie<10'],
+                    features: {
+                        'nesting-rules': true
+                    }
+                },
+                cssNano: { autoprefixer: false }
+            }
+        },
 
-		javascript: {
-			input: "src/js/**/*.js",
-			exclude: "!src/js/vendor/**/*.js",
-			output: "js/",
-			rename: {
-				suffix: ".min"
-			}
-		},
+        javascript: {
+            input: 'src/js/**/*.js',
+            exclude: '!src/js/vendor/**/*.js',
+            output: 'js/',
+            rename: {
+                suffix: '.min'
+            }
+        },
 
-		images: {
-			input: "src/images/**/*.{jpg,jpeg,gif,png,svg}",
-			output: "images/",
-			rename: {
-				suffix: "-min"
-			}
-		}
-	};
+        images: {
+            input: 'src/images/**/*.{jpg,jpeg,gif,png,svg}',
+            output: 'images/',
+            rename: {
+                suffix: '-min'
+            }
+        }
+    };
 
-	return config;
+    return config;
 };
